d6e648226c48c807db8b60bd5e542d9b3ae1da9b
natto447 <nattooliveira7@gmail.com>
2025-11-27 23:20:46 -0300

    modificando o ci.yml e o providers.tf dynamodb.tf ec2.tf s3.tf variables.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,1 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "meu-bucket-exemplo-${random_id.sufixo.hex}"
-}
 
-resource "random_id" "sufixo" {
-  byte_length = 4
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,1 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
+
